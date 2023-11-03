e6163ebe2acea6448a7de7abc66762ada976301b
rayveneja <rayvenejenningsallen@gmail.com>
2023-11-03 05:04:09 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form'); 
+    const emailInput = document.querySelector('input[type="email"]'); 
+    const messageDiv = document.querySelector('.message');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+  
+      const email = emailInput.value.trim();
+  
+      if (email === '') {
+        messageDiv.textContent = 'Please enter a valid email address.';
+      } else {
+        messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+      }
+    });
+  });
+  
